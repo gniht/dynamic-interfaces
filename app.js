bd66468b055183bdae576f1b7646de76b0c49039
Jason Thomas <jasonthomasjat@gmail.com>
2021-02-17 16:33:28 -0800

    basic slideshow with resizing on current image

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+let indexOffset = 0;
+
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+updateSlides(document.querySelector('.slideshow').children);
+
+left.addEventListener('click', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  indexOffset -= 1;
+  updateSlides(document.querySelector('.slideshow').children);
+});
+right.addEventListener('click', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  indexOffset += 1;  
+  updateSlides(document.querySelector('.slideshow').children);
+});
+
+function updateSlides (slidesArr) {
+  let selectedIndex = Math.abs(indexOffset%slidesArr.length);
+  for (let i = 0; i < slidesArr.length; i++) {
+    if (i === selectedIndex) {
+      slidesArr[i].classList.remove('hide');
+      slidesArr[i].classList.add('show');
+    } else {
+      slidesArr[i].classList.remove('show');
+      slidesArr[i].classList.add('hide');
+    }
+  }   
+}
+
 
